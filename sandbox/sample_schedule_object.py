471511ca4c6122eca1030b3d4789e1b1e2774313
Joel Bender <joel@carrickbender.com>
2017-11-10 02:11:53 -0500

    turn this into a server application

--- sandbox/sample_schedule_object.py
+++ sandbox/sample_schedule_object.py
@@ -7,9 +7,9 @@ Sample Schedule Object
 from functools import partial
 
 from bacpypes.debugging import bacpypes_debugging, ModuleLogger
-from bacpypes.consolelogging import ArgumentParser
+from bacpypes.consolelogging import ConfigArgumentParser
 
-from bacpypes.core import run_once
+from bacpypes.core import run
 from bacpypes.task import RecurringTask
 
 from bacpypes.primitivedata import Null, Integer, Real
@@ -17,10 +17,16 @@ from bacpypes.constructeddata import ArrayOf, AnyAtomic
 from bacpypes.basetypes import DailySchedule, TimeValue
 from bacpypes.object import register_object_type, WritableProperty, ScheduleObject
 
+from bacpypes.app import BIPSimpleApplication
+from bacpypes.service.device import LocalDeviceObject
+
 # some debugging
 _debug = 0
 _log = ModuleLogger(globals())
 
+#
+#   SampleScheduleObject
+#
 
 @bacpypes_debugging
 @register_object_type(vendor_id=999)
@@ -36,16 +42,20 @@ class SampleScheduleObject(ScheduleObject):
 
         self._task = SampleScheduleInterpreter(self)
 
+#
+#   SampleScheduleInterpreter
+#
 
 @bacpypes_debugging
 class SampleScheduleInterpreter(RecurringTask):
 
     def __init__(self, sched_obj):
         if _debug: SampleScheduleInterpreter._debug("__init__ %r", sched_obj)
-        RecurringTask.__init__(self, interval=1)
+        RecurringTask.__init__(self, interval=1000)
 
         # reference the schedule object to update
         self.sched_obj = sched_obj
+        # self.install_task()
 
     def process_task(self):
         if _debug: SampleScheduleInterpreter._debug("process_task")
@@ -58,59 +68,92 @@ def something_changed(thing, old_value, new_value):
     print("%r changed from %r to %r" % (thing, old_value, new_value))
 
 #
+#   __main__
 #
-#
 
-# parse the command line arguments
-parser = ArgumentParser(usage=__doc__)
-args = parser.parse_args()
+def main():
+    global args
 
-if _debug: _log.debug("initialization")
-if _debug: _log.debug("    - args: %r", args)
+    # parse the command line arguments
+    parser = ConfigArgumentParser(description=__doc__)
 
-# make a schedule object with an integer value
-so1 = SampleScheduleObject(
-    objectIdentifier=1, objectName='Schedule 1 (integer)',
-    presentValue=Integer(8),
-    weeklySchedule=ArrayOf(DailySchedule)([
-        DailySchedule(
-            daySchedule=[
-                TimeValue(time=(8,0,0,0), value=Integer(8)),
-                TimeValue(time=(14,0,0,0), value=Null()),
-                TimeValue(time=(17,0,0,0), value=Integer(42)),
-                TimeValue(time=(0,0,0,0), value=Null()),
-                ]),
-        ] * 7),
-    scheduleDefault=Integer(0),
-    )
-_log.debug("    - so1: %r", so1)
+    # parse the command line arguments
+    args = parser.parse_args()
 
-so2 = SampleScheduleObject(
-    objectIdentifier=2, objectName='Schedule 2 (real)',
-    presentValue=Real(73.5),
-    weeklySchedule=ArrayOf(DailySchedule)([
-        DailySchedule(
-            daySchedule=[
-                TimeValue(time=(9,0,0,0), value=Real(78.0)),
-                TimeValue(time=(10,0,0,0), value=Null()),
-                ]),
-        ] * 7),
-    scheduleDefault=Real(72.0),
-    )
-_log.debug("    - so2: %r", so2)
+    if _debug: _log.debug("initialization")
+    if _debug: _log.debug("    - args: %r", args)
 
-# add very simple monitors
-so1._property_monitors['presentValue'].append(
-    partial(something_changed, "so1"),
-    )
-so2._property_monitors['presentValue'].append(
-    partial(something_changed, "so2"),
-    )
+    # make a device object
+    this_device = LocalDeviceObject(
+        objectName=args.ini.objectname,
+        objectIdentifier=('device', int(args.ini.objectidentifier)),
+        maxApduLengthAccepted=int(args.ini.maxapdulengthaccepted),
+        segmentationSupported=args.ini.segmentationsupported,
+        vendorIdentifier=int(args.ini.vendoridentifier),
+        )
 
-# test it
-so1.presentValue = Integer(1)
+    # make a sample application
+    this_application = BIPSimpleApplication(this_device, args.ini.address)
 
-# change the element
-so2.WriteProperty('presentValue', Real(80.0))
+    # get the services supported
+    services_supported = this_application.get_services_supported()
+    if _debug: _log.debug("    - services_supported: %r", services_supported)
 
+    # let the device object know
+    this_device.protocolServicesSupported = services_supported.value
+
+    # make a schedule object with an integer value
+    so1 = SampleScheduleObject(
+        objectIdentifier=('schedule', 1),
+        objectName='Schedule 1 (integer)',
+        presentValue=Integer(8),
+        weeklySchedule=ArrayOf(DailySchedule)([
+            DailySchedule(
+                daySchedule=[
+                    TimeValue(time=(8,0,0,0), value=Integer(8)),
+                    TimeValue(time=(14,0,0,0), value=Null()),
+                    TimeValue(time=(17,0,0,0), value=Integer(42)),
+                    TimeValue(time=(0,0,0,0), value=Null()),
+                    ]),
+            ] * 7),
+        scheduleDefault=Integer(0),
+        )
+    _log.debug("    - so1: %r", so1)
+    this_application.add_object(so1)
+
+    so2 = SampleScheduleObject(
+        objectIdentifier=('schedule', 2),
+        objectName='Schedule 2 (real)',
+        presentValue=Real(73.5),
+        weeklySchedule=ArrayOf(DailySchedule)([
+            DailySchedule(
+                daySchedule=[
+                    TimeValue(time=(9,0,0,0), value=Real(78.0)),
+                    TimeValue(time=(10,0,0,0), value=Null()),
+                    ]),
+            ] * 7),
+        scheduleDefault=Real(72.0),
+        )
+    _log.debug("    - so2: %r", so2)
+    this_application.add_object(so2)
+
+    # add very simple monitors
+    so1._property_monitors['presentValue'].append(
+        partial(something_changed, "so1"),
+        )
+    so2._property_monitors['presentValue'].append(
+        partial(something_changed, "so2"),
+        )
+
+    # make sure they are all there
+    _log.debug("    - object list: %r", this_device.objectList)
+
+    _log.debug("running")
+
+    run()
+
+    _log.debug("fini")
+
+if __name__ == "__main__":
+    main()
 
